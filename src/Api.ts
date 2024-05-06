df425d06b3ef0f07fec344e3665c5a4a05d67041
vinz86 <vincenzo.disalvatore@gmail.com>
2024-05-06 21:17:23 +0200

    Aggiunti status http

--- src/Api.ts
+++ src/Api.ts
@@ -1,125 +1,136 @@
-import { http } from './modules/Http.module';
+import { http } from './modules/Http.module'
 import type { ApiObject, ApiRequestConfig, ApiResponse, Endpoint, Environments } from './ApiModels'
-import { AxiosClient, FetchClient } from './ApiClients';
+import { AxiosClient, FetchClient } from './ApiClients'
 
-let defaultClient: string = "axios";
-let defaultEnvironment: string = 'dev';
-let useStore: boolean = false;
-let apiPrefix: Environments = { dev: "", test: "", prod: "" };
-let apiBaseUrl: Environments = { dev: "", test: "", prod: "" };
-let apiEndpoints: Endpoint|null = null;
+let defaultClient: string = 'axios'
+let defaultEnvironment: string = 'dev'
+let useStore: boolean = false
+let apiPrefix: Environments = { dev: '', test: '', prod: '' }
+let apiBaseUrl: Environments = { dev: '', test: '', prod: '' }
+let apiEndpoints: Endpoint | null = null
 
 export const api: ApiObject = {
   setDefaultClient(client: string): void {
-    defaultClient = client;
+    defaultClient = client
   },
 
   getDefaultClient(): string {
-    return defaultClient;
+    return defaultClient
   },
 
   setDefaultEnvironment(environment: string): void {
-    defaultEnvironment = environment;
+    defaultEnvironment = environment
   },
 
   getDefaultEnvironment(): string {
-    return defaultEnvironment;
+    return defaultEnvironment
   },
 
   setApiBaseUrl(baseUrl: Environments): void {
-    apiBaseUrl = baseUrl;
+    apiBaseUrl = baseUrl
   },
 
   getApiBaseUrl(): Environments {
-    return apiBaseUrl;
+    return apiBaseUrl
   },
 
   getCurrentApiBaseUrl(): string {
     if (defaultEnvironment in this.getApiBaseUrl()) {
-      return apiBaseUrl[defaultEnvironment as keyof Environments];
+      return apiBaseUrl[defaultEnvironment as keyof Environments]
     } else {
-      return '';
+      return ''
     }
   },
 
   setApiPrefix(prefix: Environments): void {
-    apiPrefix = prefix;
+    apiPrefix = prefix
   },
 
   getCurrentApiPrefix(): string {
     if (this.getDefaultEnvironment() in this.getApiPrefix()) {
-      return apiPrefix[defaultEnvironment as keyof Environments];
+      return apiPrefix[defaultEnvironment as keyof Environments]
     } else {
-      return '';
+      return ''
     }
   },
 
   getApiPrefix(): Environments {
-    return apiPrefix;
+    return apiPrefix
   },
 
   getUseStore(): boolean {
-    return useStore;
+    return useStore
   },
 
   setUseStore(flag: boolean): void {
-    useStore = flag;
+    useStore = flag
   },
 
   getEndpoints(): Endpoint {
-    return <Endpoint>apiEndpoints;
+    return <Endpoint>apiEndpoints
   },
 
   setEndpoints(endpoints: any): void {
-    apiEndpoints = endpoints;
+    apiEndpoints = endpoints
   },
 
   getEndpoint(name: string | void): string | void {
     if (name && apiEndpoints && name in apiEndpoints && this.getDefaultEnvironment() in apiEndpoints[name]) {
-      return apiEndpoints[name][this.getDefaultEnvironment() as keyof Environments];
+      return apiEndpoints[name][this.getDefaultEnvironment() as keyof Environments]
     } else {
-      throw new Error('Endpoint non valido o non definito');
+      throw new Error('Endpoint non valido o non definito')
     }
   },
 
-  getUrlParams (params: {}){
+  getUrlParams(params: {}) {
     if (params && Object.keys(params).length > 0) {
-      return `?${new URLSearchParams(params).toString()}`;
+      return `?${new URLSearchParams(params).toString()}`
     }
   },
 
-  async request<T>(config: ApiRequestConfig): Promise<ApiResponse<T>|any> {
+  async request<T>(config: ApiRequestConfig): Promise<ApiResponse<T> | any> {
     try {
-      if (this.getDefaultClient() === "axios") {
-        return await AxiosClient<T>(config);
-      } else if (this.getDefaultClient() === "fetch") {
-        return await FetchClient<T>(config);
+      if (this.getDefaultClient() === 'axios') {
+        return await AxiosClient<T>(config)
+      } else if (this.getDefaultClient() === 'fetch') {
+        return await FetchClient<T>(config)
       } else {
-        return await FetchClient<T>(config);
+        return await FetchClient<T>(config)
       }
     } catch (error: any) {
       // La richiesta è stata effettuata ma il server ha risposto con uno status di errore
       if (error.response) {
-        console.error('Errore nella richiesta API:', error.response.status);
-
-        if (error.response.status === 404) {
-          console.error('Risorsa non trovata');
+        console.error('Errore nella richiesta API:', error.response.status)
+        if (error.response.status === 400) {
+          console.error('Bad Request')
+        } else if (error.response.status === 401) {
+          console.error("Unauthorized")
+        } else if (error.response.status === 403) {
+          console.error('Forbidden')
+        } else if (error.response.status === 404) {
+          console.error('Not Found')
+        } else if (error.response.status === 422) {
+          console.error('Unprocessable Entity')
         } else if (error.response.status === 500) {
-          console.error('Errore interno del server');
+          console.error('Internal Server Error')
+        } else if (error.response.status === 502) {
+          console.error('Bad Gateway')
+        } else if (error.response.status === 503) {
+          console.error('Service Unavailable')
         } else {
           // Aggiungere ulteriori else if per gestire altri status
         }
       }
       // La richiesta è stata effettuata ma non c'è stata risposta dal server
       else if (error.request) {
-        console.error('Nessuna risposta dal server:', error.request);
+        console.error('No response:', error.request)
       } else {
-        console.error('Errore durante la preparazione della richiesta:', error.message);
+        console.error('Request error:', error.message)
       }
-      throw error;
+      throw error
     }
   },
 
   // includo solo il modulo http, gli altri moduli verranno aggiunti tramite il plugin in modo dinamico
   http
-};
+}
