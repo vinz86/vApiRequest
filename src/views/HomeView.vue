4c4ff102c5460dccdbc066d9fa3877fecced076f
vinz86 <vincenzo.disalvatore@gmail.com>
2024-05-11 18:05:58 +0200

    Aggiornato metodo di esempio

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,115 +1,124 @@
 <script setup lang="ts">
-import { inject } from 'vue'
+import {inject, onMounted} from 'vue'
+import {courses} from "@/modules/courses/Courses.module";
+import {coursesEndpoints} from "@/modules/courses/Courses.endpoints";
+import {todos} from "@/modules/todos/Todos.module";
+import {todosEndpoints} from "@/modules/todos/Todos.endpoints";
+import { objectToFixedLengthHash } from 'vapirequest/src/ApiUtils';
+
 const api: any = inject('api');
 const useApiStore: any = inject('useApiStore');
-let testStore;
-    /*
-    // Prova Http con store
-    async function testApi() {
-      try {
-        api.setUseStore(true);
-        api.setDefaultClient('axios')
-        api.setApiBaseUrl({test:"", prod:"", dev: 'https://jsonplaceholder.typicode.com' });
 
-        let response = await api.todos.getTodo({pathParams: { id: 1 }});
-        testStore = await useApiStore().getData({ module: 'todos', endpoint:'/todos/1'});
-        console.log('Dato GET store:', testStore);
-        console.log('Risposta GET:', response);
+onMounted(async ()=> {
+  const callConfig = {
+    defaultClient: 'axios',
+    defaultEnvironment: 'dev',
+    useStore: false,
+    apiBaseUrl: {
+      dev: 'https://jsonplaceholder.typicode.com',
+      test: 'http://localhost:5173',
+      prod: 'http://localhost:5173'
+    },
+    apiPrefix: {dev: '', test: '', prod: ''},
+    tokenKey: 'tokenApi',
+    authType: 'basic'
+  }
 
-        const responseGetParams = await api.http.get('comments', { queryParams: { postId: "1" } });
-        testStore = await useApiStore().getData({ module: 'http', endpoint: `/comments${api.getUrlParams({ postId: "1" })}` } );
-        console.log('Dato GET store:', testStore);
-        console.log('Risposta GET params:', responseGetParams);
+  await httpTest(callConfig);
 
-        const responsePost = await api.http.post('posts', { data: { userId: 1, id: 1, title: 'delectus aut autem', completed: false } });
-        console.log('Risposta POST', responsePost);
+})
 
-        const responsePatch = await api.http.patch('posts/1', { data: { userId: 1, id: 1, title: 'delectus aut autem', completed: false } });
-        console.log('Risposta PATCH', responsePatch);
-
-        const responsePut = await api.http.put('posts/1', { data: { userId: 1, id: 1, title: 'delectus aut autem', completed: false } });
-        console.log('Risposta PUT', responsePut);
-
-        const responseDelete = await api.http.delete('posts/1');
-        console.log('Risposta DELETE:', responseDelete);
-
-        testStore = await useApiStore().getAllData('http')
-        console.log('getAllData http:', testStore);
-
-        testStore = await useApiStore().getAllData('todos')
-        console.log('getAllData todos:', testStore);
-
-        useApiStore().clearData('http')
-        testStore = await useApiStore().getAllData('http')
-        console.log('getAllData store after clear:', testStore);
-
-        // Simulo errore richiesta
-        api.setDefaultClient('fetch')
-        await api.http.get('commentjhnlks', { queryParams: { postId: "1", authenticate: true } });
-        await api.todos.getTodo();
-
-      } catch (error) {
-        // Gestione degli errori globali
-        console.log('Errore durante l\'esecuzione di testApi():' + error);
-      }
-    }
-    */
-
-// Prova Http con store
-async function testApi() {
+async function httpTest(callConfig: any) {
   try {
-    api.setUseStore(true);
-    api.setApiBaseUrl({test:"", prod:"", dev: 'https://jsonplaceholder.typicode.com' });
+    let reqConfigGET = {responseType: 'arraybuffer', queryParams: {postId: "1"}, pathParams: {id: 1}};
+    let reqConfigPOST = {pathParams: {id: 1}, data: {postId: 1}};
+    let reqConfigPATCH = {pathParams: {id: 1}, data: {postId: 1, appostId: 1}};
+    let reqConfigPUT = {pathParams: {id: 1}, data: {postId: 1, rimpostId: 1}};
+    let reqConfigDELETE = {pathParams: {id: 1}};
 
-    //Test modulo
-    let response = await api.todos.getTodo({pathParams: { id: 1 }});
-    console.log('getTodo', response);
+    api.setDefaultClient(callConfig.defaultClient || 'fetch');
+    api.setUseStore(callConfig.useStore || true);
+    api.setDefaultEnvironment(callConfig.defaultEnvironment || true);
+    api.setApiBaseUrl(callConfig.apiBaseUrl || true);
+    api.setApiPrefix(callConfig.apiPrefix || true);
+    api.setTokenKey(callConfig.tokenKey || true);
+    api.setAuthType(callConfig.authType || true);
 
+    // GET
+    console.info('GET');
+    let testGET = await api.http.get('posts/{id}', reqConfigGET);
+    let urlStoreGET = api.getRequestUrl('/posts/1', reqConfigGET.queryParams);
+    let storeGET = useApiStore().getData({module: 'http', method: 'get', endpoint: urlStoreGET});
+    console.log('  Risposta GET:', testGET);
+    console.log('  Key Store GET:', urlStoreGET+'-GET'+objectToFixedLengthHash(reqConfigGET.data || {} ))
+    console.log('  Store GET:', storeGET);
+    console.log('');
 
-    // Test xhr
-    api.setDefaultClient('xhr')
-    const reqConfig = {
-      queryParams: { postId: "1"},
-      pathParams: { id: 1 },
-      responseType: "arraybuffer"
-    }
+    // HEAD
+    console.info('HEAD');
+    let testHEAD = await api.http.head('comments', reqConfigGET);
+    console.log('  Risposta HEAD:', testHEAD);
+    console.log('');
 
-    const testXhr = await api.http.get('comments', reqConfig);
-    console.log('test xhr', testXhr);
-    //testStore = useApiStore().getData({ module: 'http', endpoint: `/comments${api.getUrlParams(reqConfig)`});
-    //console.log('Dato GET store:', testStore);
-
-    testStore = await useApiStore().getAllData('http')
-    console.log('getAllData http:', testStore);
-
-    // Test soap
-    // api.setDefaultClient('soap')
-    // api.setApiBaseUrl({test:"", prod:"", dev: '/public' });
-    // api.setUseStore(true);
-    // const testSoap = await api.http.post('xml/SoapData.xml', {action:'GetUserDetailsResponse'});
-    // console.log('test soap', testSoap);
-
-
-    api.setDefaultClient('fetch')
-    api.setToken("TEST_TOKEN")
-    const responsePost = await api.http.post('posts', { authenticate: true, data: { userId: 1, id: 1, title: 'delectus aut autem' } });
-    console.log('Risposta POST', responsePost);
-
-    // Simulo errore richiesta
-    await api.http.get('commentjhnlks', {
-      queryParams: { postId: "1"},
-      pathParams: { id: 1 },
-      authenticate: true
+    // POST
+    console.info('POST');
+    let testPOST = await api.http.post('posts', reqConfigPOST);
+    let urlStorePOST = api.getRequestUrl('/posts');
+    let storePOST = useApiStore().getData({
+      module: 'http',
+      method: 'post',
+      endpoint: urlStorePOST,
+      payload: reqConfigPOST.data
     });
+    console.log('  Risposta POST:', testPOST);
+    console.log('  Key Store POST:', urlStorePOST+'-POST'+objectToFixedLengthHash(reqConfigPOST.data))
+    console.log('  Store POST:', storePOST);
+    console.log('');
 
+    // PATCH
+    console.info('PATCH');
+    let testPATCH = await api.http.patch('posts/{id}', reqConfigPATCH);
+    let urlStorePATCH = api.getRequestUrl('/posts/1');
+    let storePATCH = useApiStore().getData({
+      module: 'http',
+      method: 'patch',
+      endpoint: urlStorePATCH,
+      payload: reqConfigPATCH.data
+    });
+    console.log('  Risposta PATCH:', testPATCH);
+    console.log('  Key Store PATCH:', urlStorePATCH+'-PATCH'+objectToFixedLengthHash(reqConfigPOST.data))
+    console.log('  Store PATCH:', storePATCH);
+    console.log('');
 
-  } catch (error) {
-    // Gestione degli errori globali
-    console.log('Errore durante l\'esecuzione di testApi():', error);
+    // PUT
+    console.info('PUT');
+    let testPUT = await api.http.put('posts/{id}', reqConfigPUT);
+    let urlStorePUT = api.getRequestUrl('/posts/1');
+    let storePUT = useApiStore().getData({
+      module: 'http',
+      method: 'put',
+      endpoint: urlStorePUT,
+      payload: reqConfigPUT.data
+    });
+    console.log('  Risposta PUT:', testPUT);
+    console.log('  Key Store PUT:', urlStorePUT+'-PUT'+objectToFixedLengthHash(reqConfigPOST.data))
+    console.log('  Store PUT:', storePUT);
+    console.log('');
+
+    // DELETE
+    console.info('DELETE');
+    let testDELETE = await api.http.delete('posts/{id}', reqConfigDELETE);
+    console.log('  Risposta DELETE:', testDELETE);
+    console.log('');
+
+    // Tutti i dati dallo store
+    const testStoreAll = await useApiStore().getAllData('http')
+    console.log('testStoreAll:', testStoreAll);
+  }
+  catch (error) {
+    console.error(`Errore durante l'esecuzione di httpTest(callConfig)`, error);
   }
 }
-
-testApi();
 
 </script>
 
