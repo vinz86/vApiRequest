ec891640e81635bb8c19b5a7b178eda620f66228
vinz86 <vincenzo.disalvatore@gmail.com>
2024-05-11 21:44:16 +0200

    Rimossi (commentati per un eventuale futuro utilizzo) endpoints statici, non più necessari.

--- src/ApiEndpoints.ts
+++ src/ApiEndpoints.ts
@@ -1,15 +1,20 @@
 import type {Endpoint} from './ApiModels';
 
-/* Endpoint statici definiti per gli ambienti dev, test e prod */
-const staticEndpoints: Endpoint = {
-    testEndpoint: {
-        dev: 't_dev',
-        test: 't_test',
-        prod: 't_prod',
-    },
-};
+export const apiEndpoints: Endpoint = {};
 
-/* Tutti gli endpoint API, inclusi quelli statici e dinamici */
-export const apiEndpoints: Endpoint = {
-    ...staticEndpoints,
-};
+/**
+ * Per aggiungere endpoints statici sostituire "export const apiEndpoints: Endpoint = {};" con:
+ */
+// /* Endpoint statici definiti per gli ambienti dev, test e prod */
+// const staticEndpoints: Endpoint = {
+//     testEndpoint: {
+//         dev: 't_dev',
+//         test: 't_test',
+//         prod: 't_prod',
+//     },
+// };
+//
+// /* Tutti gli endpoint API, inclusi quelli statici e dinamici */
+// export const apiEndpoints: Endpoint = {
+//     ...staticEndpoints,
+// };
